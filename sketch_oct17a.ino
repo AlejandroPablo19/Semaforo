8fd86ac8f5bb1f47fe5f33b87413d2f4fec62167
AlejandroPablo19 <alejandropabloacos19@gmail.com>
2024-11-14 08:51:15 -0600

    Add files via upload

---
+++ sketch_oct17a.ino
@@ -0,0 +1,27 @@
+int ldr = A5;
+const int ledblanco = 10;
+int valor;
+int dt = 1000;
+void setup(){
+    Serial.begin(9600);
+    pinMode(ldr, INPUT);
+    pinMode(ledblanco, OUTPUT);
+    
+  
+}
+ void loop(){
+
+  int valor = analogRead(ldr);
+  Serial.println(valor);
+  delay(dt);
+  
+
+  if(valor>300){
+    digitalWrite(ledblanco, HIGH);
+ 
+  }
+  if(valor<300){
+    digitalWrite(ledblanco, LOW);
+ 
+  }
+ }
